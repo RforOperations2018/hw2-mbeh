f44516d22525f2852862f7fa2474cfacecaa42e0
Min <mbeh@andrew.cmu.edu>
2018-09-14 23:45:46 -0400

    Loaded movie dataset and added extra UI configurations eg sidebar width

--- hw2_mbeh.R
+++ hw2_mbeh.R
@@ -7,3 +7,21 @@ library(reshape2)
 library(dplyr)
 library(plotly)
 library(RColorBrewer)
+
+# ui configuration
+sidebarWidth <- 300
+plotlyDefaultFont <- list(
+  family = "Arial",
+  size = 18,
+  color = "#000000"
+)
+
+# load dataset (to be filtered by reactive function later)
+movies.load <- read.csv('movies.csv', stringsAsFactors=FALSE)
+genres <- c('Action', 'Animation', 'Comedy', 'Crime', 'Drama', 'Family', 'Fantasy', 
+            'History', 'Mystery', 'Romance', 'Science Fiction', 'Thriller', 'War')
+# note: these genres do not represent all genres in the dataset, but are particularly popular ones
+pdf(NULL)
+
+# Run the application 
+shinyApp(ui = ui, server = server, enableBookmarking = "url")
